e65cfd24ed91864d57710d910e8274af8e5fca8c
Diego Bartolome <diego_bartolome@telefonica.net>
2021-01-04 15:18:31 +0100

    nuevo codigo javascript

--- js/script.js
+++ js/script.js
@@ -1,4 +1,22 @@
+function pulsarindiaboton() {
+    var x = document.getElementById("india");
+    if (x.checked==false) {
+        x.checked = false;
+        document.getElementById("india1").style.backgroundColor = "white";
+
+    } else {
+        x.checked = true;
+        document.getElementById("india1").style.backgroundColor = "#f8e0b9";
+    }
+}
 function pulsarindia() {
     var x = document.getElementById("india");
-    x.checked = true;
+    if (x.checked==true) {
+        x.checked = false;
+        document.getElementById("india1").style.backgroundColor = "white";
+
+    } else {
+        x.checked = true;
+        document.getElementById("india1").style.backgroundColor = "#f8e0b9";
+    }
 }
