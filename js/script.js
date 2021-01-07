2683c2f0855f2c8e5a430796ecd3af83835b5fa9
Diego Bartolome <diego_bartolome@telefonica.net>
2021-01-07 11:19:55 +0100

    javascript terminado

--- js/script.js
+++ js/script.js
@@ -20,3 +20,122 @@ function pulsarindia() {
         document.getElementById("india1").style.backgroundColor = "#f8e0b9";
     }
 }
+function pulsarmxboton() {
+    var x = document.getElementById("mexico");
+    if (x.checked==false) {
+        x.checked = false;
+        document.getElementById("mexico1").style.backgroundColor = "white";
+
+    } else {
+        x.checked = true;
+        document.getElementById("mexico1").style.backgroundColor = "#f8e0b9";
+    }
+}
+function pulsarmx() {
+    var x = document.getElementById("mexico");
+    if (x.checked==true) {
+        x.checked = false;
+        document.getElementById("mexico1").style.backgroundColor = "white";
+
+    } else {
+        x.checked = true;
+        document.getElementById("mexico1").style.backgroundColor = "#f8e0b9";
+    }
+}
+
+
+function pulsarnuevazboton() {
+    var x = document.getElementById("nuevaz");
+    if (x.checked==false) {
+        x.checked = false;
+        document.getElementById("nuevaz1").style.backgroundColor = "white";
+
+    } else {
+        x.checked = true;
+        document.getElementById("nuevaz1").style.backgroundColor = "#f8e0b9";
+    }
+}
+function pulsarnuevaz() {
+    var x = document.getElementById("nuevaz");
+    if (x.checked==true) {
+        x.checked = false;
+        document.getElementById("nuevaz1").style.backgroundColor = "white";
+
+    } else {
+        x.checked = true;
+        document.getElementById("nuevaz1").style.backgroundColor = "#f8e0b9";
+    }
+}
+
+
+function pulsarperuboton() {
+    var x = document.getElementById("peru");
+    if (x.checked==false) {
+        x.checked = false;
+        document.getElementById("peru1").style.backgroundColor = "white";
+
+    } else {
+        x.checked = true;
+        document.getElementById("peru1").style.backgroundColor = "#f8e0b9";
+    }
+}
+function pulsarperu() {
+    var x = document.getElementById("peru");
+    if (x.checked==true) {
+        x.checked = false;
+        document.getElementById("peru1").style.backgroundColor = "white";
+
+    } else {
+        x.checked = true;
+        document.getElementById("peru1").style.backgroundColor = "#f8e0b9";
+    }
+}
+
+
+function pulsarislandiaboton() {
+    var x = document.getElementById("islandia");
+    if (x.checked==false) {
+        x.checked = false;
+        document.getElementById("islandia1").style.backgroundColor = "white";
+
+    } else {
+        x.checked = true;
+        document.getElementById("islandia1").style.backgroundColor = "#f8e0b9";
+    }
+}
+function pulsarislandia() {
+    var x = document.getElementById("islandia");
+    if (x.checked==true) {
+        x.checked = false;
+        document.getElementById("islandia1").style.backgroundColor = "white";
+
+    } else {
+        x.checked = true;
+        document.getElementById("islandia1").style.backgroundColor = "#f8e0b9";
+    }
+}
+
+
+
+function pulsarjaponboton() {
+    var x = document.getElementById("japon");
+    if (x.checked==false) {
+        x.checked = false;
+        document.getElementById("japon1").style.backgroundColor = "white";
+
+    } else {
+        x.checked = true;
+        document.getElementById("japon1").style.backgroundColor = "#f8e0b9";
+    }
+}
+function pulsarjapon() {
+    var x = document.getElementById("japon");
+    if (x.checked==true) {
+        x.checked = false;
+        document.getElementById("japon1").style.backgroundColor = "white";
+
+    } else {
+        x.checked = true;
+        document.getElementById("japon1").style.backgroundColor = "#f8e0b9";
+    }
+}
